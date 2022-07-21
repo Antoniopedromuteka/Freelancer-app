4b652da4162e1e4c7618d8e03028dbd6e4d45e41
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-07-21 10:39:12 +0100

    finalizando o baner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import { Banner } from './src/Components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +11,19 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | freelancer</title>
       </Head>
+      <main className={styles.container}>
 
-      
+       <Banner/>
+
+       <div className="Subtitle-home" id="Sobre">
+            <h2>Sobre nós</h2>
+            <p>Freelancer, um benefício para todos</p>
+        </div>
+
+
+
+      </main>
+
            
      </>
   )
